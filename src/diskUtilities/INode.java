79d0faa6620b272a00d666a0abbc7144c879019c
captain-yuca <manuel.baez@upr.edu>
2017-03-23 21:15:36 -0400

    Added getters and setters for INode and skeleton for testers.

--- src/diskUtilities/INode.java
+++ src/diskUtilities/INode.java
@@ -1,7 +1,53 @@
 package diskUtilities;
 
 public class INode {
-	int fileSize;
-	byte fileType;
-	int firstBlockIndex;
+	private byte fileType;
+	private int fileSize;
+	private int firstBlockIndex;
+	
+	public INode(byte fileType,int fileSize, int firstBlockIndex){
+		this.fileType=fileType;
+		this.fileSize=fileSize;
+		this.firstBlockIndex=firstBlockIndex;
+	}
+	
+	
+	/**
+	 * @return the fileSize
+	 */
+	public int getFileSize() {
+		return fileSize;
+	}
+	/**
+	 * @param fileSize the fileSize to set
+	 */
+	public void setFileSize(int fileSize) {
+		this.fileSize = fileSize;
+	}
+	/**
+	 * @return the fileType
+	 */
+	public byte getFileType() {
+		return fileType;
+	}
+	/**
+	 * @param fileType the fileType to set
+	 */
+	public void setFileType(byte fileType) {
+		this.fileType = fileType;
+	}
+	/**
+	 * @return the firstBlockIndex
+	 */
+	public int getFirstBlockIndex() {
+		return firstBlockIndex;
+	}
+	/**
+	 * @param firstBlockIndex the firstBlockIndex to set
+	 */
+	public void setFirstBlockIndex(int firstBlockIndex) {
+		this.firstBlockIndex = firstBlockIndex;
+	}
+	
+	
 }
---
+++ src/testers/INodesTester.java
@@ -0,0 +1,14 @@
+package testers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class INodesTester {
+
+	@Test
+	public void TestImplementation() {
+		fail("Not yet implemented");
+	}
+
+}
